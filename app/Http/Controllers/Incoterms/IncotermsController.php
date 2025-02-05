0567fb98595ed2d7fe28ae0d9b058003e74811f0
Mark Anthony Aguilar <markanthonyaguilar@digits.ph>
2025-02-05 12:20:30 +0800

    update: add incoterms submaster

--- app/Http/Controllers/Incoterms/IncotermsController.php
+++ app/Http/Controllers/Incoterms/IncotermsController.php
@@ -1,18 +1,128 @@
 <?php
-                            namespace App\Http\Controllers\Incoterms;
-                            use App\Helpers\CommonHelpers;
-                            use App\Http\Controllers\Controller;
-                            use Illuminate\Http\Request;
-                            use Illuminate\Http\RedirectResponse;
-                            use Illuminate\Support\Facades\Auth;
-                            use Illuminate\Support\Facades\Session;
-                            use Inertia\Inertia;
-                            use Inertia\Response;
-                            use DB;
 
-                            class IncotermsController extends Controller{
-                                public function getIndex(){
-                                    return Inertia("Incoterms/Incoterms");
-                                }
-                            }
-                        ?>
+namespace App\Http\Controllers\Incoterms;
+
+use App\Helpers\CommonHelpers;
+use App\Http\Controllers\Controller;
+use App\Models\Incoterms;
+use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+use Inertia\Inertia;
+use Inertia\Response;
+use DB;
+
+class IncotermsController extends Controller
+{
+
+    private $sortBy;
+    private $sortDir;
+    private $perPage;
+
+    public function __construct() {
+        $this->sortBy = request()->get('sortBy', 'incoterms.created_at');
+        $this->sortDir = request()->get('sortDir', 'desc');
+        $this->perPage = request()->get('perPage', 10);
+    }
+
+    public function getAllData(){
+        $query = Incoterms::query()->with(['getCreatedBy', 'getUpdatedBy']);
+        $filter = $query->searchAndFilter(request());
+        $result = $filter->orderBy($this->sortBy, $this->sortDir);
+        return $result;
+    }
+
+    public function getIndex()
+    {
+        if(!CommonHelpers::isView()) {
+            return Inertia::render('Errors/RestrictionPage');
+        }
+
+        $data = [];
+        $data['tableName'] = 'incoterms';
+        $data['page_title'] = 'Incoterms';
+        $data['incoterms'] = self::getAllData()->paginate($this->perPage)->withQueryString();
+        $data['queryParams'] = request()->query();
+     
+        return Inertia::render("Incoterms/Incoterms", $data);
+    }
+
+    public function create(Request $request){
+
+        $validatedFields = $request->validate([
+            'incoterms_code' => 'required|string|max:3|unique:incoterms,incoterms_code',
+            'incoterms_description' => 'required|string|max:30|unique:incoterms,incoterms_description',
+        ]);
+
+        try {
+
+            Incoterms::create([
+                'incoterms_code' => $validatedFields['incoterms_code'], 
+                'incoterms_description' => $validatedFields['incoterms_description'],   
+                'status' => 'ACTIVE',
+                'created_by' => CommonHelpers::myId(),
+            ]);
+    
+            return back()->with(['message' => 'Incoterms Creation Success!', 'type' => 'success']);
+
+        }
+
+        catch (\Exception $e) {
+            CommonHelpers::LogSystemError('Incoterms', $e->getMessage());
+            return back()->with(['message' => 'Incoterms Creation Failed!', 'type' => 'error']);
+        }
+        
+       
+    }
+
+    public function update(Request $request){
+
+        $validatedFields = $request->validate([
+            'incoterms_code' => 'required|string|max:3',
+            'incoterms_description' => 'required|string|max:30',
+            'status' => 'required|string',
+        ]);
+
+        try {
+    
+            $incoterms = Incoterms::find($request->id);
+
+            if (!$incoterms) {
+                return back()->with(['message' => 'Incoterms not found!', 'type' => 'error']);
+            }
+    
+            $incotermsCodeExist = Incoterms::where('incoterms_code', $request->incoterms_code)->exists();
+            $incotermsDescriptionExist = Incoterms::where('incoterms_description', $request->incoterms_description)->exists();
+
+            if ($request->incoterms_code !== $incoterms->incoterms_code) {
+                if (!$incotermsCodeExist) {
+                    $incoterms->incoterms_code = $validatedFields['incoterms_code'];
+                } else {
+                    return back()->with(['message' => 'Incoterms code already exists!', 'type' => 'error']);
+                }
+            }
+            if ($request->incoterms_description !== $incoterms->incoterms_description) {
+                if (!$incotermsDescriptionExist) {
+                    $incoterms->incoterms_description = $validatedFields['incoterms_description'];
+                } else {
+                    return back()->with(['message' => 'Incoterms Description already exists!', 'type' => 'error']);
+                }
+            }
+    
+            $incoterms->status = $validatedFields['status'];
+            $incoterms->updated_by = CommonHelpers::myId();
+            $incoterms->updated_at = now();
+    
+            $incoterms->save();
+    
+            return back()->with(['message' => 'Incoterms Updating Success!', 'type' => 'success']);
+        }  
+
+        catch (\Exception $e) {
+
+            CommonHelpers::LogSystemError('Incoterms', $e->getMessage());
+            return back()->with(['message' => 'Incoterms Updating Failed!', 'type' => 'error']);
+        }
+    }
+}
---
+++ resources/js/Pages/Incoterms/IncotermsAction.jsx
@@ -0,0 +1,164 @@
+import React from 'react'
+import Button from '../../Components/Table/Buttons/Button';
+import { useTheme } from '../../Context/ThemeContext';
+import { useToast } from '../../Context/ToastContext';
+import useThemeStyles from '../../Hooks/useThemeStyles';
+import InputComponent from '../../Components/Forms/Input';
+import { router, useForm } from '@inertiajs/react';
+import DropdownSelect from '../../Components/Dropdown/Dropdown';
+
+const IncotermsAction = ({action, onClose, updateData}) => {
+    const { theme } = useTheme();
+    const { handleToast } = useToast();
+    const { primayActiveColor, textColorActive, buttonSwalColor } = useThemeStyles(theme);
+
+    const { data, setData, processing, reset, post, errors } = useForm({
+        id: "" || updateData.id,
+        incoterms_code: "" || updateData.incoterms_code,
+        incoterms_description: "" || updateData.incoterms_description,
+        status: "" || updateData.status,
+    });
+
+    const statuses = [
+        {
+            id: 'ACTIVE',
+            name:'ACTIVE',
+        },
+        {
+            id: 'INACTIVE',
+            name:'INACTIVE',
+        },
+    ]
+
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        Swal.fire({
+            title: `<p class="font-poppins text-3xl" >Do you want ${action == 'Add' ? 'add' : 'update'} Incoterms?</p>`,
+            showCancelButton: true,
+            confirmButtonText: `${action == 'Add' ? 'Confirm' : 'Update'}`,
+            confirmButtonColor: buttonSwalColor,
+            icon: 'question',
+            iconColor: buttonSwalColor,
+            reverseButtons: true,
+        }).then(async (result) => {
+            if (result.isConfirmed) {
+
+                if (action == 'Add'){
+                    post('incoterms/create', {
+                        onSuccess: (data) => {
+                            const { message, type } = data.props.auth.sessions;
+                            handleToast(message, type);
+                            router.reload({ only: ["incoterms"] });
+                            reset();
+                            onClose();
+                        },
+                        onError: (error) => {
+                        }
+                    });
+                }
+                else{
+                    post('incoterms/update', {
+                        onSuccess: (data) => {
+                            const { message, type } = data.props.auth.sessions;
+                            handleToast(message, type);
+                            router.reload({ only: ["incoterms"] });
+                            reset();
+                            onClose();
+                        },
+                        onError: (error) => {
+                        }
+                    });
+                }
+                
+            }
+        });
+    }
+
+  return (
+    <form onSubmit={handleFormSubmit} className='space-y-2'>
+        {/* INCOTERMS CODE */}
+        <InputComponent
+            name="incoterms_code"
+            value={data.incoterms_code}
+            disabled={action === 'View'}
+            placeholder="Enter Color Code"
+            onChange={(e)=> setData("incoterms_code", e.target.value)}
+        />
+        {(errors.incoterms_code) && (
+            <div className="font-poppins text-xs font-semibold text-red-600">
+                {errors.incoterms_code}
+            </div>
+        )}
+        {/* INCOTERMS DESCRIPTION */}
+        <InputComponent
+            name="incoterms_description"
+            value={data.incoterms_description}
+            disabled={action === 'View'}
+            placeholder="Enter Color Description"
+            onChange={(e)=> setData("incoterms_description", e.target.value)}
+        />
+        {(errors.incoterms_description) && (
+            <div className="font-poppins text-xs font-semibold text-red-600">
+                {errors.incoterms_description}
+            </div>
+        )}
+        {action == 'Update' && 
+            <>
+                <DropdownSelect
+                    placeholder="Choose Status"
+                    selectType="react-select"
+                    defaultSelect="Select Status"
+                    onChange={(selectedOption) => setData("status", selectedOption?.value)}
+                    name="status"
+                    options={statuses}
+                    value={data.status ? { label: data.status, value: data.status } : null}
+                />
+                {(errors.status) && (
+                    <div className="font-poppins text-xs font-semibold text-red-600">
+                        {errors.status}
+                    </div>
+                )}
+            </>
+        }
+
+        {action == "View" && 
+            <div className='flex items-center space-x-2'>
+                <div className={`block text-sm font-bold ${theme === 'bg-skin-black' ? ' text-gray-400' : 'text-gray-700'}  font-poppins`}>
+                    Status
+                </div>
+                <div className={`select-none ${data.status == 'ACTIVE' ? 'bg-status-success': 'bg-status-error'} mb-2 text-sm font-poppins font-semibold py-1 px-3 text-center text-white rounded-full mt-2`}>
+                    {data.status}
+                </div>
+            </div>
+        }
+        
+        
+        {action !== 'View' && 
+            <div className='flex justify-end'>
+                <Button
+                    type="button"
+                    extendClass={`${theme === 'bg-skin-white' ? primayActiveColor : theme}`}
+                    fontColor={textColorActive}
+                    disabled={processing}
+                >
+                {processing ? 
+                    (
+                        action === "Add" ? 'Submitting' : 'Updating'
+                    ) 
+                    : 
+                    (
+                        <span>
+                            <i className="fa-solid fa-plus mr-1"></i> {action === "Add" ? 'Add Incoterms' : 'Update Incoterms'}
+                        </span>
+                    )
+                }
+                </Button>  
+            </div>
+        }
+        
+    </form>
+  )
+}
+
+export default IncotermsAction
