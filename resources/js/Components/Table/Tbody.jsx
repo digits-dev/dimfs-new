b85a33ade91428f83c8b338b461a2b7afa9348a5
Mark Anthony Aguilar <markanthonyaguilar@digits.ph>
2025-02-11 15:12:23 +0800

    update: update tbody

--- resources/js/Components/Table/Tbody.jsx
+++ resources/js/Components/Table/Tbody.jsx
@@ -14,10 +14,12 @@ const Tbody = ({data, children}) => {
             {children}
         </tbody>
         : <tbody>
-            <div className={`${theme === 'bg-skin-black' ? 'bg-skin-black' : 'bg-white'} absolute flex  w-full h-52 items-center justify-center`}>
-                <img className='w-16 h-16 mr-2 mb-5' src='/images/others/empty-box.png'/>
-                <p className='font-poppins mb-2 font-medium text-gray-400'>No Data Available</p>
-            </div>
+            <tr className={`${theme === 'bg-skin-black' ? 'bg-skin-black' : 'bg-white'} absolute flex  w-full h-52 items-center justify-center`}>
+                <td className=' flex  h-52 items-center justify-center'>
+                    <img className='w-12 h-12 md:w-16 md:h-16 mr-2 mb-5' src='/images/others/empty-box.png'/>
+                    <p className='font-poppins mb-2 text-sm md:text-base font-medium text-gray-400'>No Data Available</p>
+                </td>
+            </tr>
         </tbody>}
     </>
   )
