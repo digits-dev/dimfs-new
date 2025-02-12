ce54e371b1f1d52f36797df3e41b087739852ae8
Piolo Arrabis <pioloarrabis@digits.ph>
2025-02-12 17:20:20 +0800

    create view

--- resources/js/Pages/TableSettings/CreateView.jsx
+++ resources/js/Pages/TableSettings/CreateView.jsx
@@ -13,7 +13,7 @@ const CreateTableSetting = ({ privileges, action_types }) => {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
 
-     const { data, setData, processing, reset, post, errors } = useForm({
+    const { data, setData, processing, reset, post, errors } = useForm({
         module_id: "",
         module_name: "",
         action_type_id: "",
@@ -25,19 +25,18 @@ const CreateTableSetting = ({ privileges, action_types }) => {
 
     const modules = [
         {
-            id: '38',
-            name:'Item Master',
+            id: "38",
+            name: "Item Master",
         },
         {
-            id: '28',
-            name:'Gashapon Item Master',
+            id: "28",
+            name: "Gashapon Item Master",
         },
-    ]
+    ];
 
-    useEffect(()=>{
+    useEffect(() => {
         console.log(data);
-    },[data]);
-
+    }, [data]);
 
     const handleCheckboxChange = (item) => {
         setData((prevData) => ({
@@ -50,7 +49,6 @@ const CreateTableSetting = ({ privileges, action_types }) => {
 
     useEffect(() => {
         if (data.module_id) {
-            
             setLoading(true);
             setError(null);
             axios
@@ -68,7 +66,6 @@ const CreateTableSetting = ({ privileges, action_types }) => {
         }
     }, [data.module_id]);
 
-
     return (
         <div className="h-full font-poppins">
             <Head title="Create Table Setting" />
@@ -84,15 +81,24 @@ const CreateTableSetting = ({ privileges, action_types }) => {
                                 placeholder="Select Module"
                                 selectType="react-select"
                                 defaultSelect="Select Module"
-                                onChange={(selectedOption) => setData((prevData) => ({
-                                    ...prevData,
-                                    module_id: selectedOption?.value,
-                                    module_name: selectedOption?.label,
-                                    checked_items: [],
-                                }))}
+                                onChange={(selectedOption) =>
+                                    setData((prevData) => ({
+                                        ...prevData,
+                                        module_id: selectedOption?.value,
+                                        module_name: selectedOption?.label,
+                                        checked_items: [],
+                                    }))
+                                }
                                 name="module_name"
                                 options={modules}
-                                value={data.module_id ? { label: data.module_name, value: data.module_id } : null}
+                                value={
+                                    data.module_id
+                                        ? {
+                                              label: data.module_name,
+                                              value: data.module_id,
+                                          }
+                                        : null
+                                }
                             />
                             {errors.module_name && (
                                 <div className="font-poppins text-xs font-semibold text-red-600">
@@ -105,10 +111,22 @@ const CreateTableSetting = ({ privileges, action_types }) => {
                                 placeholder="Select Action Type"
                                 selectType="react-select"
                                 defaultSelect="Select Action Type"
-                                onChange={(selectedOption) => setData("action_type_id", selectedOption?.value)}
+                                onChange={(selectedOption) =>
+                                    setData(
+                                        "action_type_id",
+                                        selectedOption?.value
+                                    )
+                                }
                                 name="action_type"
                                 options={action_types}
-                                value={data.action_type_id ? { label: data.action_type_id, value: data.action_type_id } : null}
+                                value={
+                                    data.action_type_id
+                                        ? {
+                                              label: data.action_type_id,
+                                              value: data.action_type_id,
+                                          }
+                                        : null
+                                }
                             />
                             {errors.action_type_id && (
                                 <div className="font-poppins text-xs font-semibold text-red-600">
@@ -121,10 +139,22 @@ const CreateTableSetting = ({ privileges, action_types }) => {
                                 placeholder="Select Privilege"
                                 selectType="react-select"
                                 defaultSelect="Select Privilege"
-                                onChange={(selectedOption) => setData("privilege_id", selectedOption?.value)}
+                                onChange={(selectedOption) =>
+                                    setData(
+                                        "privilege_id",
+                                        selectedOption?.value
+                                    )
+                                }
                                 name="privilege Name"
                                 options={privileges}
-                                value={data.privilege_id ? { label: data.privilege_id, value: data.privilege_id } : null}
+                                value={
+                                    data.privilege_id
+                                        ? {
+                                              label: data.privilege_id,
+                                              value: data.privilege_id,
+                                          }
+                                        : null
+                                }
                             />
                             {errors.privilege_id && (
                                 <div className="font-poppins text-xs font-semibold text-red-600">
@@ -135,37 +165,39 @@ const CreateTableSetting = ({ privileges, action_types }) => {
 
                         {/* CARD 2 */}
                         <div className="w-[70%] flex-1 flex flex-col min-h-full border rounded-lg p-4 border-gray-300">
-                            {loading ? 
+                            {loading ? (
                                 <div className="w-full h-full flex items-center justify-center">
-                                    <LoadingIcon/>
+                                    <LoadingIcon />
                                 </div>
-
-                                :
+                            ) : (
                                 <>
-                                    {moduleData ? 
+                                    {moduleData ? (
                                         moduleData?.map((item, index) => (
                                             <CheckboxWithText
-                                                id={`checkbox-${index}`}         
-                                                type="checkbox"             
+                                                id={`checkbox-${index}`}
+                                                type="checkbox"
                                                 name="exampleCheckbox"
                                                 text={item}
-                                                textColor="text-black" 
-                                                handleClick={() => handleCheckboxChange(item)} 
-                                                isChecked={data.checked_items.includes(item)}        
-                                                disabled={false}       
+                                                textColor="text-black"
+                                                handleClick={() =>
+                                                    handleCheckboxChange(item)
+                                                }
+                                                isChecked={data.checked_items.includes(
+                                                    item
+                                                )}
+                                                disabled={false}
                                             />
-                                        )) 
-                                    :
-
-                                    <div className="select-none w-full h-full flex items-center justify-center border border-dashed rounded-lg border-gray-300">
-                                        <p className="font-semibold text-gray-300">Please Select Module</p>
-                                    </div>
-                                    
-                                    }
+                                        ))
+                                    ) : (
+                                        <div className="select-none w-full h-full flex items-center justify-center border border-dashed rounded-lg border-gray-300">
+                                            <p className="font-semibold text-gray-300">
+                                                Please Select Module
+                                            </p>
+                                        </div>
+                                    )}
                                 </>
-                            }
+                            )}
                             {error && <p className="text-red-500">{error}</p>}
-
                         </div>
                     </div>
                 </form>
