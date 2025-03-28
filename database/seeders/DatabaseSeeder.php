9f18367705ded798585dfcb6ce1a34221db524b2
Mark Anthony Aguilar <markanthonyaguilar@digits.ph>
2025-03-28 10:19:11 +0800

    update: adjust seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,12 +13,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // $this->call([AdmSeeders::class]);
-        // $this->call([AdmModules::class]);
-        // $this->call([AdmAdminMenusSeeder::class]);
-        // $this->call([AdmMenusSeeder::class]);
-        // $this->call([AdmMenuPrivileges::class]);
-        // $this->call([AdmPrivileges::class]);
-        // $this->call([AdmUsersSeeders::class]);
+        $this->call([AdmSeeders::class]);
+        $this->call([AdmModules::class]);
+        $this->call([AdmAdminMenusSeeder::class]);
+        $this->call([AdmMenusSeeder::class]);
+        $this->call([AdmMenuPrivileges::class]);
+        $this->call([AdmPrivileges::class]);
+        $this->call([AdmUsersSeeders::class]);
+        $this->call([AdmSettingsSeeder::class]);
     }
 }
